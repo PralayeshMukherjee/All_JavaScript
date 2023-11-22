7a38a7067b13ffce07fb047b985ecddbe83ef8c4
pralayesh <rajmukherjee2807@gmail.com>
2023-11-22 18:55:56 +0530

    all variables

--- VariableAndDatatype.js
+++ VariableAndDatatype.js
@@ -1,3 +1,25 @@
 // to print anything in JavaScript
 console.log("Hello World");
 console.log("My name is Raj");
+////////////////////////// Variable //////////////////////
+// String
+Fullname = "Pralayesh Mukherjee";
+console.log(Fullname);
+// integer
+age = 19;
+console.log(age);
+// floating
+price = 23.42;
+console.log(price);
+// null value
+x = null;
+console.log(x);
+// undifined value
+y = undefined;
+console.log(y);
+// undifined means we don't know the value and null means we know the value which is empty😀
+// boolean
+isCorrect1 = false;
+isCorrect2 = true;
+console.log(isCorrect1, isCorrect2);
+// JavaScript is a Dynamically Typed Language means we don't need to defined the variable type in front of the variable so we can easily change any data type to another data type.
